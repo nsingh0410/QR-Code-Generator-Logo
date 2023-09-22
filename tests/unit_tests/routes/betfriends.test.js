8dcdf0b9b0eaa24170ede73c617ece2991df0cd9
Nikhil Singh <singhn@skybroadcast.net.local>
2023-09-22 11:11:51 +1000

    Added new unit tests to routes.

---
+++ tests/unit_tests/routes/betfriends.test.js
@@ -0,0 +1,38 @@
+const path = require('path'); // Add this line to import the path module
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { appRootDirectory } = require('../../testConstants');
+const app = require(path.join(appRootDirectory, 'src', 'app.js'));
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('Betfriends API', () => {
+    it('should generate a QR code for betfriends', function (done) {
+      // Set a longer timeout (e.g., 5000ms)
+      this.timeout(5000);
+      let imageDir = appRootDirectory + '/../images';
+    
+      chai.request(app)
+        .post('/generateqr/betfriends')
+        .send({
+            "date": "2023-09-14",
+            "trackName": "NORTHFIELD-PARK",
+            "bravoCode": "NFP",
+            "racingType": "H",
+            "racingNumber": "1",
+            "logoImagePath": "images/logos/tab-logo-white.png",
+            "qrSize": 512,
+            "logoSize": 150,
+            "outputDirectory": imageDir
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          // You can add more assertions as needed
+          done();
+        });
+    });
+  
+    // Add other test cases here
+  });
---
+++ tests/unit_tests/routes/generate-file.test.js
@@ -0,0 +1,33 @@
+const path = require('path'); // Add this line to import the path module
+const appDirectory = path.resolve(__dirname);
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { appRootDirectory } = require('../../testConstants');
+const app = require(path.join(appRootDirectory, 'src', 'app.js'));
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('generate-file API', () => {
+    it('should generate a QR code for generate file', function (done) {
+      // Set a longer timeout (e.g., 5000ms)
+      this.timeout(5000);
+      let imageDir = appRootDirectory + '/../images';
+    
+      chai.request(app)
+        .post('/generateqr/generate-file')
+        .send({
+            "text" : "https://www.skyracing.com.au",
+            "logoImagePath": "images/logos/tab-logo-white.png",
+            "qrSize": 512,
+            "logoSize": 150,
+            "outputDirectory": imageDir
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          // You can add more assertions as needed
+          done();
+        });
+    });
+  });
